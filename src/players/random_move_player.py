25154751bce75d5c3606f2f112ed38779fe1451a
Korius668 <korius668@gmail.com>
2026-06-06 18:31:36 +0200

    update random move player

--- src/players/random_move_player.py
+++ src/players/random_move_player.py
@@ -1,10 +1,14 @@
 import random
+import chess
+
 from core.player_base import BasePlayer
 
 class RandomMovePlayer(BasePlayer):
     """Model 'Mock' wykonujący losowe legalne ruchy."""
-    def get_move(self, board):
-        legal_moves = list(board.legal_moves)
-        return random.choice(legal_moves) if legal_moves else None
     
+    def get_move(self, board: chess.Board, legal_moves: list=None) -> chess.Move:
+        if legal_moves is None:
+            legal_moves = list(board.legal_moves)
+        
+        return random.choice(legal_moves) if legal_moves else None
     
